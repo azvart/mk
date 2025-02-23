89c0a7ccdd6911f8a239ac590f19cc8105e3873b
vladgasanov <vlad.gasanov@zing.coach>
2025-02-23 23:41:26 +0100

    MK-1 - create sign in and sign up form

---
+++ src/app/signin/page.tsx
@@ -0,0 +1,1 @@
+import SignInForm from "@/app/components/signin";export default async function SignInPage(){    return <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">        <div className="w-full max-w-sm">            <SignInForm />        </div>    </div>}
