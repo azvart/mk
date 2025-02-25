20c0253d88823853f257ad888ef88b5a8d7bb675
vladgasanov <vlad.gasanov@zing.coach>
2025-02-26 00:20:33 +0100

    MK-2 - changes route path

--- src/lib/actions/signin.ts
+++ src/lib/actions/signin.ts
@@ -1,1 +1,22 @@
-'use server'export async function signin(data:FormData) {    try{        const email = data.get('email');        const request = await fetch(`${process.env.SERVER_URL}/account/sign-in`, {            method: 'POST',            body: JSON.stringify({ email}),            headers: {                'Content-Type': 'application/json'            }        })        if(request.ok){            return request.json()        }else {            throw new Error(request.statusText)        }    }catch(error){        throw new Error(JSON.stringify(error))    }}
+'use server'
+export async function signin(data:FormData) {
+    try{
+        const email = data.get('email');
+        const request = await fetch(`${process.env.SERVER_URL}/account/sign-in`, {
+            method: 'POST',
+            body: JSON.stringify({ email}),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        if(request.ok){
+            return request.json()
+        }else {
+            throw new Error(request.statusText)
+        }
+
+    }catch(error){
+        throw new Error(JSON.stringify(error))
+    }
+
+}
--- src/lib/actions/signup.ts
+++ src/lib/actions/signup.ts
@@ -1,1 +1,25 @@
-'use server'export async function signup(data:FormData){    try {        const email = data.get('email');        const password = data.get('password');        const request = await fetch(`${process.env.SERVER_URL}/account/sign-up`, {            method: 'POST',            headers: {                'Content-Type': 'application/json'            },            body: JSON.stringify({                email,                password            })        })        if(request.ok){            return request.json()        }else {            throw new Error(request.statusText)        }    }catch(error){        throw new Error(JSON.stringify(error))    }}
+'use server'
+export async function signup(data:FormData){
+    try {
+        const email = data.get('email');
+        const password = data.get('password');
+
+        const request = await fetch(`${process.env.SERVER_URL}/account/sign-up`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                email,
+                password
+            })
+        })
+        if(request.ok){
+            return request.json()
+        }else {
+            throw new Error(request.statusText)
+        }
+    }catch(error){
+        throw new Error(JSON.stringify(error))
+    }
+}
